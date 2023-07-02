229167be6f3b63cbe89b5b25fe8b4934ea11e3dd
David Anniwell <davidanniwell@gmail.com>
2023-07-02 15:58:59 +0100

    Started System Setup window

---
+++ PulsarUI/Views/SystemSetupWindow.axaml.cs
@@ -0,0 +1,21 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PulsarUI.Views;
+
+public partial class SystemSetupWindow : Window
+{
+    public SystemSetupWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
